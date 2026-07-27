1060fed0090569b8cfb93ae16889270bfe24d36b
recker895 <66128735+recker895@users.noreply.github.com>
2026-07-27 22:49:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,7 @@
 
    
 
-async function search() {
-    const query = document.getElementById("query").value;
 
-    const res = await fetch("/search", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({query})
-    });
 
     const data = await res.json();
 
